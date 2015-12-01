2e3abcdaf99a9f9111731ad94d361daa62eab60d
resteve <resteve@zikzakmedia.com>
2015-12-01 10:51:35 +0100

    Tests

---
+++ tests/test_account_bank_statement_counterpart.py
@@ -0,0 +1,23 @@
+# This file is part of the account_bank_statement_counterpart module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+import unittest
+import doctest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+
+
+class AccountBankStatementCounterpartTestCase(ModuleTestCase):
+    'Test Account Bank Statement Counterpart module'
+    module = 'account_bank_statement_counterpart'
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        AccountBankStatementCounterpartTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_bank_statement.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    return suite
