3bbb7f6b80c6bb9c92df9463c0296ae97a479093
resteve <resteve@zikzakmedia.com>
2015-12-02 09:52:38 +0100

    FIX loading Tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,7 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# This file is part account_bank_statement_counterpart module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 
-from .test_account_bank_statement import suite
+from .test_account_bank_statement_counterpart import suite
 
 __all__ = ['suite']
