6e6bbe9f039d5f7deec5ae4fd9ba7b5301911f68
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-21 00:25:59 +0100

    Pyflakes fixes.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,14 @@
 # This file is part account_bank_statement_counterpart the COPYRIGHT file at
 # the top level of this repository contains the full copyright notices and
 # license terms.
-
 from trytond.pool import Pool
-from .statement import *
+from . import statement
 
 
 def register():
     Pool.register(
-        Move,
-        MoveLine,
-        StatementLine,
-        Reconciliation,
+        statement.Move,
+        statement.MoveLine,
+        statement.StatementLine,
+        statement.Reconciliation,
         module='account_bank_statement_counterpart', type_='model')
